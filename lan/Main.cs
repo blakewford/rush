8f73de443240fafc47c854e349d8fb89e4b94b49
Blake W. Ford <blakewford@gmail.com>
2019-02-23 01:31:38 -0600

    Setup basic chart bitmap.

--- lan/Main.cs
+++ lan/Main.cs
@@ -1,10 +1,37 @@
 using System;
+using System.Drawing;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 public class Monitor
 {
+    private static void InitializeBitmap(ref Bitmap bitmap)
+    {
+        Color background = Color.White;
+        Rectangle rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+        System.Drawing.Imaging.BitmapData data = bitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.WriteOnly, bitmap.PixelFormat);
+
+        int size  = Math.Abs(data.Stride) * bitmap.Height;
+
+        IntPtr p = data.Scan0;
+        byte[] raw = new byte[size];
+
+//        Console.WriteLine((data.Stride/data.Width) == 4);
+
+        for(int i = 0; i < size; i+=4)
+        {
+            raw[i+0] = background.B;
+            raw[i+1] = background.G;
+            raw[i+2] = background.R;
+            raw[i+3] = background.A;
+        }
+
+        System.Runtime.InteropServices.Marshal.Copy(raw, 0, p, size);
+        bitmap.UnlockBits(data);
+    }
+
     public static void MonitorLoop(IPAddress address)
     {
         try
@@ -40,6 +67,16 @@ public class Monitor
 
     public static int Main(String[] args)
     {
+        Bitmap bitmap = new Bitmap(512, 512);
+        new Thread(() => {
+            while(true)
+            {
+                InitializeBitmap(ref bitmap);
+                bitmap.Save("Chart.png");
+                Thread.Sleep(1000);
+            }
+        }).Start();
+
         foreach(IPAddress address in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
         {
             if(address.AddressFamily == AddressFamily.InterNetwork)
